1fffb88ea884caa327b7df144c5da7be1ba20376
Kayke simao <kayke.py@gmail.com>
2026-09-16 16:11:33 -0300

    First commit

---
+++ src/main/java/com/simao/perfumehub/PerfumehubApplication.java
@@ -0,0 +1,13 @@
+package com.simao.perfumehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfumehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfumehubApplication.class, args);
+	}
+
+}

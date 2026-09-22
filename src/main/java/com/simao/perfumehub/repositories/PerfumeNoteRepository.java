5907da4626f4c43dd83cbe32956ab7ce6ab78204
Kayke simao <kayke.py@gmail.com>
2026-09-22 19:47:04 -0300

    feat(repository): add custom queries to PerfumeNoteRepository
    Add spring data JPA repository methods for PerfumeNote entity.
    
    - Add  to check for existing associations
    - Add  to retrieve all notes related to a specific perfume

--- src/main/java/com/simao/perfumehub/repositories/PerfumeNoteRepository.java
+++ src/main/java/com/simao/perfumehub/repositories/PerfumeNoteRepository.java
@@ -1,8 +1,14 @@
 package com.simao.perfumehub.repositories;
 
+import com.simao.perfumehub.entities.PerfumeNote;
+import com.simao.perfumehub.entities.enums.NoteType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface PerfumeNote extends JpaRepository<> {
+public interface PerfumeNoteRepository extends JpaRepository<PerfumeNote, Long> {
+    boolean existsByPerfumeIdAndNoteIdAndType(Long perfumeId, Long noteId, NoteType type);
+    List<PerfumeNote> findAllByPerfumeId(Long id);
 }

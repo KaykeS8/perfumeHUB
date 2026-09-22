f8ce78f9dbcbaff681a33f0cdc380217410d9d90
Kayke simao <kayke.py@gmail.com>
2026-09-22 14:43:30 -0300

    fix(olfactoryNote/mapper, OlfactoryNote/servce) Unnecessary imports removed from the service and added annotation @mapping to ignore id in dto

--- src/main/java/com/simao/perfumehub/mapper/OlfactoryNoteMapper.java
+++ src/main/java/com/simao/perfumehub/mapper/OlfactoryNoteMapper.java
@@ -4,9 +4,11 @@ import com.simao.perfumehub.dtos.olfactoryNote.OlfactoryNoteRequestDto;
 import com.simao.perfumehub.dtos.olfactoryNote.OlfactoryNoteResponseDto;
 import com.simao.perfumehub.entities.OlfactoryNote;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface OlfactoryNoteMapper {
+    @Mapping(target = "id", ignore = true)
     OlfactoryNote toEntity(OlfactoryNoteRequestDto dto);
     OlfactoryNoteResponseDto toDto(OlfactoryNote olfactoryNote);
 }

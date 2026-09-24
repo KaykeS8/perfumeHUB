ddc02649d064d09eb07aaa104372200066b0abd2
Kayke simao <kayke.py@gmail.com>
2026-09-24 17:26:21 -0300

    feat: add perfume filtering by olfactory note

--- src/main/java/com/simao/perfumehub/specifications/PerfumeSpecifications.java
+++ src/main/java/com/simao/perfumehub/specifications/PerfumeSpecifications.java
@@ -1,8 +1,13 @@
 package com.simao.perfumehub.specifications;
 
+import com.simao.perfumehub.entities.OlfactoryNote;
 import com.simao.perfumehub.entities.Perfume;
+import com.simao.perfumehub.entities.PerfumeNote;
 import com.simao.perfumehub.entities.enums.Genre;
+import jakarta.persistence.criteria.JoinType;
 import org.springframework.data.jpa.domain.Specification;
+import jakarta.persistence.criteria.Join;
+
 
 import java.math.BigDecimal;
 
@@ -25,7 +30,7 @@ public class PerfumeSpecifications {
                 concentration == null ? null : cb.equal(root.get("concentration"), Genre.valueOf(concentration.toUpperCase()));
     }
 
-    public static Specification<Perfume> priceBetaween(BigDecimal priceMin, BigDecimal priceMax) {
+    public static Specification<Perfume> priceBetween(BigDecimal priceMin, BigDecimal priceMax) {
         return (root, query, cb) -> {
             if (priceMin == null && priceMax == null) return null;
             if (priceMin == null) return cb.lessThanOrEqualTo(root.get("price"), priceMax);
@@ -37,6 +42,18 @@ public class PerfumeSpecifications {
     public static Specification<Perfume> nameContains(String name) {
         return (root, query, cb) ->
                 name == null ? null : cb.like(cb.lower(root.get("name")), "%" + name.toLowerCase() + "%");
+    }
+
+    public static Specification<Perfume> hasNote(String noteName) {
+        return (root, query, cb) -> {
+            if (noteName == null) return null;
+            Join<Perfume, PerfumeNote> perfumeNoteJoin = root.join("perfumeNotes", JoinType.INNER);
+            Join<PerfumeNote, OlfactoryNote> noteJoin = perfumeNoteJoin.join("note", JoinType.INNER);
+
+            query.distinct(true);
+
+            return cb.equal(cb.lower(noteJoin.get("name")), noteName.toLowerCase());
+        };
     }
 
 }
